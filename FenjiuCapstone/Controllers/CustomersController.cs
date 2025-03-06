804700b7ebe025b14e7ca1b7ba80dcddae36e7d0
KingOfChelsea <xuzhenyu3333@gmail.com>
2025-03-06 17:46:08 +0800

    1.创建Customer对象，用于服务Customers接口 2.创建Customers接口，获取全部买家信息

---
+++ FenjiuCapstone/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using sales_managers.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FenjiuCapstone.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        [HttpGet]
+        [Route("api/customers/get-customers")]
+        public HttpResponseMessage GetCustomers()
+        {
+            List<Dictionary<string, object>> customers = new List<Dictionary<string, object>>();
+            string sql = "SELECT * FROM Customers";
+            using (var reader = DbAccess.read(sql))
+            {
+                while (reader.Read())
+                {
+                    var customer = new Dictionary<string, Object>
+                    {
+                        ["CustomerID"] = reader["CustomerID"],
+                        ["CustomerName"] = reader["CustomerName"],
+                        ["ContactNumber"] = reader["ContactNumber"],
+                        ["Email"] = reader["Email"],
+                        ["Address"] = reader["Address"],
+                        ["CreatedAt"] = reader["CreatedAt"]
+                    };
+                    customers.Add(customer);
+                };
+            }
+            return JsonResponseHelper.CreateJsonResponse(new { success = false,data=customers});
+        }
+
+
+    }
+}
---
+++ FenjiuCapstone/Models/Sales/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FenjiuCapstone.Models.Sales
+{
+    /// <summary>
+    /// 买家客户类
+    /// </summary>
+    public class Customer
+    {
+        /// <summary>
+        /// 客户ID
+        /// </summary>
+        public int CustomerID { get; set; }
+
+        /// <summary>
+        /// 客户姓名
+        /// </summary>
+        public string CustomerName { get; set; }
+
+        /// <summary>
+        /// 客户电话
+        /// </summary>
+        public string ContactNumber { get; set; }
+
+        /// <summary>
+        /// 客户邮箱
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// 客户地址
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// 客户创建时间
+        /// </summary>
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
