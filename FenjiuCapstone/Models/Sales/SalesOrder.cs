09f2d836f27092d76fab32e7435172d9b0bdbc50
KingOfChelsea <xuzhenyu3333@gmail.com>
2025-03-07 15:00:58 +0800

    添加订单管理 API必要文件 Signed-off-by: KingOfChelsea <xuzhenyu3333@gmail.com>

---
+++ FenjiuCapstone/Models/Sales/SalesOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FenjiuCapstone.Models.Sales
+{
+    /// <summary>
+    /// 销售订单(唯一订单)
+    /// </summary>
+    public class SalesOrder
+    {
+        public int OrderID { get; set; }
+        public int CustomerID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public string Status { get; set; }
+        public List<SalesOrderDetail> OrderDetails { get; set; } = new List<SalesOrderDetail>();
+    }
+}
---
+++ FenjiuCapstone/Models/Sales/SalesOrderDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FenjiuCapstone.Models.Sales
+{
+    public class SalesOrderDetail
+    {
+        public int DetailID { get; set; }
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal SubTotal { get; set; }
+    }
+}
