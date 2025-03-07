2969a02ae0c15beec9766395914a42391c3936d0
KingOfChelsea <xuzhenyu3333@gmail.com>
2025-03-07 11:51:37 +0800

    创建客户枚举权限类，用于判断权限是否充足

---
+++ FenjiuCapstone/Utils/EnumAccessModCustomer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FenjiuCapstone.Utils
+{
+    public class EnumAccessModCustomer
+    {
+        /// <summary>
+        /// 枚举类用于判断删除销售人员权限
+        /// </summary>
+        public enum ModCustomer
+        {
+            /// <summary>
+            /// 销售经理
+            /// </summary>
+             SaleManagers = 4,
+
+             /// <summary>
+             /// 系统管理员
+             /// </summary>
+             System = 1
+
+        }
+    }
+}
