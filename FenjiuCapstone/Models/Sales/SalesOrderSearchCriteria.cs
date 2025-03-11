40b55bf26bc461681902a0cbfa2ca89e912fe965
KingOfChelsea <xuzhenyu3333@gmail.com>
2025-03-11 11:04:36 +0800

    创建订单查询对象，继承了Customer对象，重载CustomerID对象
    Signed-off-by: KingOfChelsea <xuzhenyu3333@gmail.com>

---
+++ FenjiuCapstone/Models/Sales/SalesOrderSearchCriteria.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FenjiuCapstone.Models.Sales
+{
+    public class SalesOrderSearchCriteria:SalesOrder
+    {
+        public new int ? CustomerID { get; set; }     // 重写CustomerID属性
+        public string CustomerName { get; set; }      // 客户名称
+        public decimal? MinTotalAmount { get; set; }  // 最小总金额
+        public decimal? MaxTotalAmount { get; set; }  // 最大总金额
+        public DateTime[] OrderDateRange { get; set; }  // 订单日期范围
+    }
+}
